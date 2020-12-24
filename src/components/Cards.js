e74e1c25180b3a30f38889caea148608db9d36f6
Frank Nguyen <fnguyen2000@hotmail.com>
2020-12-24 18:42:58 +1100

    Finalise card designs and add new sections to Home

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,23 +1,97 @@
 import React from "react";
 import "../styles/Cards.css";
 
-const Card = ({ title, link, subheading, image, description }) => {
+export const Card = ({
+  title,
+  link,
+  button,
+  subheading,
+  image,
+  description,
+  tags,
+}) => {
   return (
     <>
       <div class="card">
-        <img class="card-image" src={image}></img>
-        <div class="card-content">
-          <h1 className="card-title">{title}</h1>
-          <p class="card-subheading">{subheading}</p>
-          <a className="card-link" href={link}>
-            <b>REPOSITORY</b>
-          </a>
-          <br />
-          <p class="card-description">{description}</p>
+        <div class="card-image-container">
+          <img class="card-image" alt="" src={image}></img>
+        </div>
+        <div class="content-container">
+          <div class="card-content">
+            <h1 className="card-title">{title}</h1>
+            <p class="card-subheading">{subheading}</p>
+            <a className="card-link" href={link}>
+              <b>{button}</b>
+            </a>
+            <br />
+            <p class="card-description">{description}</p>
+            <div class="techstack-container">
+              {tags.map((tag) => (
+                <div class="tag-container">
+                  <a href={tag.link} class="tag">
+                    {tag.title}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default Card;
+export const ShortCard = ({
+  title,
+  link,
+  button,
+  subheading,
+  image,
+  description,
+  tags,
+}) => {
+  return (
+    <>
+      <div class="short-card" style={{ height: "100%" }}>
+        <div class="card-image-container" style={{ width: "50%" }}>
+          <img
+            class="card-image"
+            alt=""
+            src={image}
+            style={{ paddingTop: "5px" }}
+          ></img>
+        </div>
+        <div
+          class="content-container"
+          style={{ width: "100%", height: "100%" }}
+        >
+          <div
+            class="card-content"
+            style={{
+              paddingLeft: "20px",
+              paddingRight: "20px",
+              height: "100%",
+            }}
+          >
+            <h1 className="card-title">{title}</h1>
+            <p class="card-subheading">{subheading}</p>
+            <a className="card-link" href={link}>
+              <b>{button}</b>
+            </a>
+            <br />
+            <p class="card-description">{description}</p>
+            <div class="techstack-container">
+              {tags.map((tag) => (
+                <div class="tag-container">
+                  <a href={tag.link} class="tag">
+                    {tag.title}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,26 +1,86 @@
 import React from "react";
-import Card from "../Cards";
+import { Card, ShortCard } from "../Cards";
 
 import "../../styles/Home.css";
 import LinkUpImage from "../../images/linkup.png";
+import VulnBinImage from "../../images/vulnbin.png";
+import CseSecImage from "../../images/cse_sec_ctf.jpg";
+import WitIbmImage from "../../images/wit_ibm.png";
 
 export default function Home() {
   return (
     <>
-      <h1 class="home-header">Collaborative Projects</h1>
-      <div class="card-container">
+      <h1 class="collab-projects-header">Collaborative Projects</h1>
+      <div class="projects-container">
         <Card
-          title="LinkUp Events"
+          title="LinkUpEvents - University Events Tracker"
           link="https://linkupevents.com.au"
+          button="LIVE DEMO"
           subheading="Backend developer"
           image={LinkUpImage}
-          description="bruh descriptoin
-          asldfkjasdfklj
-          asdflkjasdfaklsjdfa
-          dfjas
-          dflkasjdfaskdjf"
+          description={`LinkUpEvents is a centralised source of events hosted by clubs and societies from Australian universities including UNSW, USyd and UniMelb.
+          Aims to help students discover new societies and brings more exposure to smaller societies.
+          LinkUpEvents is actively working with UNSW Arc to better support student academic and social life.`}
+          tags={[
+            { link: "https://reactjs.org/", title: "React" },
+            { link: "https://www.javascript.com/", title: "JavaScript" },
+            { link: "https://aws.amazon.com/", title: "AWS" },
+            { link: "https://www.python.org/", title: "Python" },
+          ]}
         />
       </div>
+      <br />
+      <h1 class="personal-projects-header">Personal Projects</h1>
+      <div class="projects-container">
+        <Card
+          title="Vulnerable binaries"
+          link="https://github.com/jjamme/vulnerable_binaries"
+          button="REPOSITORY"
+          subheading="C Developer"
+          image={VulnBinImage}
+          description={`A collection of binary programs written in C.
+          Vulnerabilities are intentionally added
+          `}
+          tags={[
+            {
+              link: "https://en.wikipedia.org/wiki/C_(programming_language)",
+              title: "C",
+            },
+          ]}
+        />
+      </div>
+      <br />
+      <h1 class="other-header">Other</h1>
+      <div class="task-container">
+        <ShortCard
+          title="UNSW CSESoc x SecSoc CTF"
+          link="https://www.facebook.com/events/csesoc-unsw/csesoc-x-secsoc-ctf/2951826274854928/"
+          button="LINK"
+          subheading="2nd Place"
+          image={CseSecImage}
+          description={``}
+          tags={[
+            {
+              link: "https://docs.pwntools.com/en/stable/",
+              title: "Python - pwntools",
+            },
+            { link: "https://github.com/pwndbg/pwndbg", title: "GDB - pwndbg" },
+          ]}
+        />
+        <ShortCard
+          title="UNSW WIT x IBM Code & Conquer"
+          link="https://www.facebook.com/events/959606607781668/"
+          button="LINK"
+          subheading="3rd Place"
+          image={WitIbmImage}
+          description={`sadf`}
+          tags={[
+            { link: "https://www.python.org/", title: "Python" },
+            { link: "", title: "Algorithms" },
+          ]}
+        />
+      </div>
+      <br />
     </>
   );
 }
